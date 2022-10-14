10f6d0b66516822421c212015d8fcc50569888c9
HenryAler <114738013+HenryAler@users.noreply.github.com>
2022-10-14 21:59:22 +0300

    reformatting the concept

--- main.py
+++ main.py
@@ -16,32 +16,18 @@ class MainProject(QtWidgets.QMainWindow):
         #  binding the input key to perform functions.
         self.setWindowIcon(QIcon('electronics_chip_wireless_gadget_smart_icon_229600.ico'))
         self.ui.btn_ravno.clicked.connect(self.calculation)
-
-        self.ui.btn_1.clicked.connect(lambda: self.ui.lineEdit.insert('1')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('1'))
-        self.ui.btn_2.clicked.connect(lambda: self.ui.lineEdit.insert('2')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('2'))
-        self.ui.btn_3.clicked.connect(lambda: self.ui.lineEdit.insert('3')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('3'))
-        self.ui.btn_4.clicked.connect(lambda: self.ui.lineEdit.insert('4')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('4'))
-        self.ui.btn_5.clicked.connect(lambda: self.ui.lineEdit.insert('5')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('5'))
-        self.ui.btn_6.clicked.connect(lambda: self.ui.lineEdit.insert('6')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('6'))
-        self.ui.btn_7.clicked.connect(lambda: self.ui.lineEdit.insert('7')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('7'))
-        self.ui.btn_8.clicked.connect(lambda: self.ui.lineEdit.insert('8')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('8'))
-        self.ui.btn_9.clicked.connect(lambda: self.ui.lineEdit.insert('9')
-                                      if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('9'))
-        self.ui.btn_zerro.clicked.connect(lambda: self.ui.lineEdit.insert('0')
-                                          if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('0'))
-        self.ui.btn_tochka.clicked.connect(lambda: self.ui.lineEdit.insert('.')
-                                           if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.insert('.'))
-        self.ui.ereise_button.clicked.connect(lambda: self.ui.lineEdit.backspace()
-                                              if self.ui.lineEdit.hasFocus() else self.ui.lineEdit_2.backspace())
-
+        self.ui.btn_1.clicked.connect(self.clik_1)
+        self.ui.btn_2.clicked.connect(self.clik_2)
+        self.ui.btn_3.clicked.connect(self.clik_3)
+        self.ui.btn_4.clicked.connect(self.clik_4)
+        self.ui.btn_5.clicked.connect(self.clik_5)
+        self.ui.btn_6.clicked.connect(self.clik_6)
+        self.ui.btn_7.clicked.connect(self.clik_7)
+        self.ui.btn_8.clicked.connect(self.clik_8)
+        self.ui.btn_9.clicked.connect(self.clik_9)
+        self.ui.btn_zerro.clicked.connect(self.clik_0)
+        self.ui.btn_tochka.clicked.connect(self.clik_point)
+        self.ui.ereise_button.clicked.connect(self.clik_backspace)
         self.ui.clear_button.clicked.connect(lambda: self.ui.lineEdit.clear())
         self.ui.clear_button.clicked.connect(lambda: self.ui.lineEdit_2.clear())
         self.ui.clear_button.clicked.connect(lambda: self.ui.lineEdit_3.clear())
@@ -49,22 +35,124 @@ class MainProject(QtWidgets.QMainWindow):
         self.ui.clear_button.clicked.connect(lambda: self.ui.lineEdit_5.clear())
         self.ui.clear_button.clicked.connect(lambda: self.ui.lineEdit.setFocus())
 
+    def clik_1(self):
+
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('1')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('1')
+        else:
+            self.ui.lineEdit_3.insert('1')
+
+    def clik_2(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('2')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('2')
+        else:
+            self.ui.lineEdit_3.insert('2')
+
+    def clik_3(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('3')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('3')
+        else:
+            self.ui.lineEdit_3.insert('3')
+
+    def clik_4(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('4')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('4')
+        else:
+            self.ui.lineEdit_3.insert('4')
+
+    def clik_5(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('5')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('5')
+        else:
+            self.ui.lineEdit_3.insert('5')
+
+    def clik_6(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('6')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('6')
+        else:
+            self.ui.lineEdit_3.insert('6')
+
+    def clik_7(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('7')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('7')
+        else:
+            self.ui.lineEdit_3.insert('7')
+
+    def clik_8(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('8')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('8')
+        else:
+            self.ui.lineEdit_3.insert('8')
+
+    def clik_9(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('9')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('9')
+        else:
+            self.ui.lineEdit_3.insert('9')
+
+    def clik_0(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('0')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('0')
+        else:
+            self.ui.lineEdit_3.insert('0')
+
+    def clik_point(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.insert('.')
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.insert('.')
+        else:
+            self.ui.lineEdit_3.insert('.')
+
+    def clik_backspace(self):
+        if self.ui.lineEdit.hasFocus():
+            self.ui.lineEdit.backspace()
+        elif self.ui.lineEdit_2.hasFocus():
+            self.ui.lineEdit_2.backspace()
+        else:
+            self.ui.lineEdit_3.backspace()
+
     def calculation(self):
-        # We calculate the cost per year, month and day with rounding to two digits
-        # and pass the value to the appropriate fields.
+
         try:
-            inupt_number1 = float(self.ui.lineEdit.text())
-            inupt_number2 = int(self.ui.lineEdit_2.text())
-            cost_year = round((inupt_number1 / inupt_number2), 2)
-            cost_month = round((inupt_number1 / inupt_number2 / 12), 2)
-            cost_day = round((inupt_number1 / inupt_number2 / 365), 2)
-            self.ui.lineEdit_3.setText(str(cost_year))
-            self.ui.lineEdit_4.setText(str(cost_month))
-            self.ui.lineEdit_5.setText(str(cost_day))
+            x = float(self.ui.lineEdit.text())  # Amount of credit
+            y = float(self.ui.lineEdit_2.text())  # Credit period
+            z = float(self.ui.lineEdit_3.text())  # Percent
+            z = z / 1200
+            j = x * z
+            a = (1 + z) ** y
+            s = 1 - (1 / a)
+            d = round(j / s, 2)
+
+            overpayment_on_the_loan = round(d * y - x, 2)
+
+            self.ui.lineEdit_4.setText(str(d))
+            self.ui.lineEdit_5.setText(str(overpayment_on_the_loan))
+
         except ZeroDivisionError:
-            self.ui.lineEdit_3.setText('"This is unacceptable."')
+            self.ui.lineEdit_4.setText('"This is unacceptable."')
         except ValueError:
-            self.ui.lineEdit_3.setText('"Only integer."')
+            self.ui.lineEdit_4.setText('"Only integer."')
 
 
 app = QtWidgets.QApplication([])
